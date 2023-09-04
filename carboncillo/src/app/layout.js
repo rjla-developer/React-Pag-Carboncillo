4529a378985d170d0abd0e7fd5a2654c01e58e87
rjluengas <rjluengas@megcloud.mx>
2023-09-03 21:34:19 -0600

    Creación del proyecto con next.js

---
+++ carboncillo/src/app/layout.js
@@ -0,0 +1,17 @@
+import './globals.css'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Carboncillo',
+  description: 'Somos una empresa de marketing digital',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="es">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
