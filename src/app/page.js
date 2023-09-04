99b77ce77432f91c61bfdfbee2403b173cf53487
rjluengas <rjluengas@megcloud.mx>
2023-09-03 22:13:22 -0600

    Se agrego React bootstrap al proyecto y se movieron los archivos para colocarlos en el orden que deben de estar

---
+++ src/app/page.js
@@ -0,0 +1,15 @@
+'use client'
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
+function Home() {
+  return (
+    <Container>
+      <Row>
+        <Col>Home</Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default Home;
