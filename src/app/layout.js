be089f26d8b4b7aded2a2d5721d43bc26ad22fc0
rjluengas <rjluengas@megcloud.mx>
2023-09-04 18:42:54 -0600

    DDiseño del navbar Home completado, se agrego bootstrap al layout y se agrego un logo provisional hasta que nos envíen el contenido estático final

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,27 @@
-import { Inter } from 'next/font/google'
+import { Inter } from "next/font/google";
+import "bootstrap/dist/css/bootstrap.min.css";
+import NavbarHome from "@/components/navbar/NavbarHome";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Carboncillo',
-  description: 'Somos una empresa de marketing digital',
-}
+  title: "Carboncillo",
+  description: "Somos una empresa de marketing digital",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
-      <body>{children}</body>
+      <body>
+        <NavbarHome />
+        {children}
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
+          integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM"
+          crossOrigin="anonymous"
+        />
+      </body>
     </html>
-  )
+  );
 }
---
+++ src/components/navbar/NavbarHome.jsx
@@ -0,0 +1,70 @@
+"use client";
+import React from "react";
+import { Navbar, Nav, NavDropdown, Container } from "react-bootstrap";
+import Image from "next/image";
+
+function NavbarHome() {
+  return (
+    <div className="fixed-top px-0">
+      {/* Menu computadora */}
+      <Navbar className="d-xl-block d-none position-absolute w-100">
+        <Container className="py-2">
+          <Navbar.Brand className="d-flex align-items-center">
+            <Image
+              className="d-inline-block align-top img-fluid w-25"
+              src={require("../../img/Logo/logo.png")}
+              alt="Picture of the author"
+            />
+          </Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link href="">
+              <p className="mb-0 text-white">Inicio</p>
+            </Nav.Link>
+            <Nav.Link>
+              <p className="mb-0 text-white">Servicios</p>
+            </Nav.Link>
+            <Nav.Link>
+              <p className="mb-0 text-white">Filosofía</p>
+            </Nav.Link>
+            <Nav.Link>
+              <p className="mb-0 text-white">Contacto</p>
+            </Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      {/* Menu Phone */}
+      <Navbar className="d-block d-xl-none" expand="xl">
+        <Container className="py-3 px-1">
+          <Navbar.Brand className="d-flex align-items-center">
+            <Image
+              className="d-inline-block align-top img-fluid"
+              width="200"
+              src={require("../../img/Logo/logo.png")}
+              alt="Picture of the author"
+            />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarNav" className="me-3 bg-light" />
+          <Navbar.Collapse id="navbarNav" className="bg-light ps-2">
+            <Nav className="mr-auto">
+              <Nav.Link href="">
+                <p className="mb-0 text-dark">Inicio</p>
+              </Nav.Link>
+              <Nav.Link>
+                <p className="mb-0 text-dark">Servicios</p>
+              </Nav.Link>
+              <Nav.Link>
+                <p className="mb-0 text-dark">Filosofía</p>
+              </Nav.Link>
+              <Nav.Link>
+                <p className="mb-0 text-dark">Contacto</p>
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
+}
+
+export default NavbarHome;
