c79c415a0ca0e5bfc9c4008611fec1856ac076b1
rjluengas <rjluengas@megcloud.mx>
2023-09-18 12:21:06 -0600

    Formato prettier en el componente de SeccServicios.jsx

--- src/components/screens/home/SeccServicios.jsx
+++ src/components/screens/home/SeccServicios.jsx
@@ -6,23 +6,24 @@ function SeccServicios() {
   const [dataItemsServices, setdataItemsServices] = useState([]);
 
   useEffect(() => {
-    fetch("http://127.0.0.1:5001/carboncillo-pag/us-central1/app/items-services")
-    .then((response) => response.json())
-    .then((response) => {
-      /* console.log("Data received:", response); */
-      setdataItemsServices(response);
-    })
-    .catch((error) => {
-      console.error("Error fetching data:", error);
-    });
-  
-    return () => {
-    }
-  }, [])
-  
+    fetch(
+      "http://127.0.0.1:5001/carboncillo-pag/us-central1/app/items-services"
+    )
+      .then((response) => response.json())
+      .then((response) => {
+        /* console.log("Data received:", response); */
+        setdataItemsServices(response);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+    return () => {};
+  }, []);
 
   return (
-    <div className="row mt-3 mt-lg-5 d-flex justify-content-center">
+    <div
+      className="row mt-3 mt-lg-5 d-flex justify-content-center"
+    >
       {dataItemsServices.map((service, index) => {
         /* console.log(service); */
         return (
