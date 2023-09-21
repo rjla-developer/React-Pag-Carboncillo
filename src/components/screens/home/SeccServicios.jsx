6122227d9a56057a18a111ed14df524edaf2d904
rjluengas <rjluengas@megcloud.mx>
2023-09-21 12:11:07 -0600

    Optimización en petición fetch en el componente de SeccServicios.jsx.

--- src/components/screens/home/SeccServicios.jsx
+++ src/components/screens/home/SeccServicios.jsx
@@ -1,29 +1,22 @@
-"use client";
-import React, { useState, useEffect } from "react";
+import React from "react";
 import ItemServicio from "./ItemServicio";
 
-function SeccServicios() {
-  const [dataItemsServices, setdataItemsServices] = useState([]);
+async function fetchData() {
+  try {
+    const response = await fetch(
+      `http://127.0.0.1:5001/carboncillo-pag/us-central1/app/items-services`
+    );
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+}
 
-  useEffect(() => {
-    fetch(
-      "http://127.0.0.1:5001/carboncillo-pag/us-central1/app/items-services"
-    )
-      .then((response) => response.json())
-      .then((response) => {
-        /* console.log("Data received:", response); */
-        setdataItemsServices(response);
-      })
-      .catch((error) => {
-        console.error("Error fetching data:", error);
-      });
-    return () => {};
-  }, []);
-
+async function SeccServicios() {
+  const dataItemsServices = await fetchData();
   return (
-    <div
-      className="row mt-3 mt-lg-5 d-flex justify-content-center"
-    >
+    <div className="row mt-3 mt-lg-5 d-flex justify-content-center">
       {dataItemsServices.map((service, index) => {
         /* console.log(service); */
         return (
